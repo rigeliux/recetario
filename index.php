999c57e5cd8a00568c048bf38b9038e16a53012e
rigel <raiders11@gmail.com>
2014-04-14 10:27:14 -0500

    agregue comentario

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
+/*mi casa es bonita*/
 	define('ENVIRONMENT', 'development');
 /*
  *---------------------------------------------------------------
